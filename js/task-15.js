81d04d7b2aa661e953bbc15de7f90e9d64eb4dbf
Vladislav-creator <valdisbeketov@gmail.com>
2023-08-19 19:24:25 +0300

    8

--- js/task-15.js
+++ js/task-15.js
@@ -1,6 +1,8 @@
-const lightbox = new SimpleLightbox(".js-list a", {
+let lightbox = new SimpleLightbox(".js-list a", {
   captionsData: "alt",
   captionDelay: 250,
   captionPosition: "bottom",
 });
-lightbox.next();
+// lightbox.on("show.simplelightbox", function () {
+//   change.simplelightbox;
+// });
